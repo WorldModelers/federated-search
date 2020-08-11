66746bc2538b2a38d16d5da1d9b9c723bbbe2f6d
Travis Hartman <63281380+travis-jat@users.noreply.github.com>
2020-08-11 12:45:30 -0700

    added endpoint

--- api/swagger_server/test/test_download_controller.py
+++ api/swagger_server/test/test_download_controller.py
@@ -11,14 +11,28 @@ from swagger_server.test import BaseTestCase
 class TestDownloadController(BaseTestCase):
     """DownloadController integration test stubs"""
 
-    def test_download_data_location_id_value_get(self):
-        """Test case for download_data_location_id_value_get
+    def test_download_data_location_dataset_id_get(self):
+        """Test case for download_data_location_dataset_id_get
 
-        Download datasets
+        Download NYU datasets
         """
         response = self.client.open(
-            '/download/{data_location}/{id_value}'.format(data_location='data_location_example', id_value='id_value_example'),
+            '/download/{data_location}/{dataset_id}'.format(data_location='data_location_example', dataset_id='dataset_id_example'),
             method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_download_variables_data_location_dataset_id_post(self):
+        """Test case for download_variables_data_location_dataset_id_post
+
+        Download ISI variables
+        """
+        body = ['\"access_to_electricity_of_population\"']
+        response = self.client.open(
+            '/download_variables/{data_location}/{dataset_id}'.format(data_location='data_location_example', dataset_id='dataset_id_example'),
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
