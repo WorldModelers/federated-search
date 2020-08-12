89b51bccc1895a4796ffa183870a00c180d497d9
Travis Hartman <63281380+travis-jat@users.noreply.github.com>
2020-08-12 10:17:16 -0700

    added CORS

--- api/swagger_server/__main__.py
+++ api/swagger_server/__main__.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
-
 import connexion
 
 from swagger_server import encoder
 
+from flask_cors import CORS
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'World Modelers Federated Search API'}, pythonic_params=True)
+    cors = CORS(app.app, supports_credentials=True)
     app.run(port=8080)
 
 
