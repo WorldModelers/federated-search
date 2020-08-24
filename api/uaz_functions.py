0b557f45a23ce54ac2e0622b047345f479fe2735
Travis Hartman <63281380+travis-jat@users.noreply.github.com>
2020-08-24 16:38:28 -0700

    added uaz concept search functions

---
+++ api/uaz_functions.py
@@ -0,0 +1,49 @@
+from requests import get,post,put,delete
+import json
+import pandas as pd
+from io import StringIO
+from IPython.display import display, HTML
+
+def urlify(endpoint, keywords, maxHits=100):
+
+    keywords = keywords.split(" ")
+    amp_keywords=[]
+
+    for words in keywords:
+
+        new_word = words.replace(" ", "%20")
+        amp_keywords.append(new_word)
+
+
+    keyword_string ='%20'.join(amp_keywords)
+
+    maxit = f'&maxHits={maxHits}'
+
+    search_url = endpoint + keyword_string + maxit
+    
+    return search_url
+
+
+#Format response result to swagger schema
+def schema_results_uaz(raw_uaz):
+    
+    schema_results = []
+    temp_dict = {}
+
+    for result in raw_uaz:
+
+        data_location = result.get('datamartId', "None")
+        dataset_id = result.get('datasetId', "None")
+        score = result.get('score', "None")
+        variable_id = result.get('variableId', "None")
+        descr = result.get('variableDescription', "None")
+    
+        temp_dict= {"data_location": data_location,
+                    "dataset_id": dataset_id,
+                    "variable_id": variable_id,
+                    "description": descr,
+                    "score": score}
+
+        schema_results.append(temp_dict)
+     
+    return schema_results
